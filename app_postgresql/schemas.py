587f79fa41c87fd12781236b4e41366bb5f4915f
Alexey Filichkin <filichkin.alexey@gmail.com>
2025-04-17 21:11:09 +0300

    schemas.py

---
+++ app_postgresql/schemas.py
@@ -0,0 +1,79 @@
+from datetime import datetime
+from typing import List
+import uuid
+from pydantic import BaseModel, EmailStr, constr
+
+
+MIN_LENGHT = 8
+
+
+class UserBaseSchema(BaseModel):
+    name: str
+    email: EmailStr
+    photo: str
+
+    class Config:
+        orm_mode = True
+
+
+class CreateUserSchema(UserBaseSchema):
+    password: str = constr(min_length=MIN_LENGHT)
+    passwordConfirm: str
+    role: str = 'user'
+    verified: bool = False
+
+
+class LoginUserSchema(BaseModel):
+    email: EmailStr
+    password: str = constr(min_length=MIN_LENGHT)
+
+
+class UserResponse(UserBaseSchema):
+    id: uuid.UUID
+    created_at: datetime
+    updated_at: datetime
+
+
+class FilteredUserResponse(UserBaseSchema):
+    id: uuid.UUID
+
+
+class PostBaseSchema(BaseModel):
+    title: str
+    content: str
+    category: str
+    image: str
+    user_id: uuid.UUID | None = None
+
+    class Config:
+        orm_mode = True
+
+
+class CreatePostSchema(PostBaseSchema):
+    pass
+
+
+class PostResponse(PostBaseSchema):
+    id: uuid.UUID
+    user: FilteredUserResponse
+    created_at: datetime
+    updated_at: datetime
+
+
+class UpdatePostSchema(BaseModel):
+    title: str
+    content: str
+    category: str
+    image: str
+    user_id: uuid.UUID | None = None
+    created_at: datetime | None = None
+    updated_at: datetime | None = None
+
+    class Config:
+        orm_mode = True
+
+
+class ListPostResponse(BaseModel):
+    status: str
+    results: int
+    posts: List[PostResponse]
